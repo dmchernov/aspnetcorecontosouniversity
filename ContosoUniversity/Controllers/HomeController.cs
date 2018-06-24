7d548c008eb5a96baa56ccbfa453c65a37ef9ba9
Dmitrii Chernov <dm.chernov@outlook.com>
2018-06-24 21:03:06 +0400

    About page has been changed to show students statistics

--- ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -1,21 +1,39 @@
-﻿using System.Diagnostics;
+﻿using ContosoUniversity.Data;
 using ContosoUniversity.Models;
+using ContosoUniversity.Models.SchoolViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ContosoUniversity.Controllers
 {
 	public class HomeController : Controller
 	{
+		private readonly SchoolContext _context;
+
+		public HomeController(SchoolContext context)
+		{
+			_context = context;
+		}
+
 		public IActionResult Index()
 		{
 			return View();
 		}
 
-		public IActionResult About()
+		public async Task<ActionResult> About()
 		{
-			ViewData["Message"] = "Your application description page.";
-
-			return View();
+			IQueryable<EnrollmentDateGroup> data =
+				from student in _context.Students
+				group student by student.EnrollmentDate into dateGroup
+				select new EnrollmentDateGroup
+				{
+					EnrollmentDate = dateGroup.Key,
+					StudentCount = dateGroup.Count()
+				};
+			return View(await data.AsNoTracking().ToListAsync());
 		}
 
 		public IActionResult Contact()
