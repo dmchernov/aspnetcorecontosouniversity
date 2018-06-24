6e7d82aa011163623bc4957f5a00d747ba6f8b2a
Dmitrii Chernov <dm.chernov@outlook.com>
2018-06-24 10:03:02 +0400

    Display attributes have been to students

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContosoUniversity.Models
 {
 	public class Student
 	{
 		public int Id { get; set; }
+
+		[DisplayName("Фамилия")]
 		public string LastName { get; set; }
+
+		[DisplayName("Имя и отчество")]
 		public string FirstMidName { get; set; }
+
+		[DisplayName("Дата зачисления")]
+		[DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
 		public DateTime EnrollmentDate { get; set; }
 
 		public ICollection<Enrollment> Enrollments { get; set; }
